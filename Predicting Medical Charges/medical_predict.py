4fe4f40e6d6b3964505fe4b15b5ea019b343e42f
Chanuka Wijesinghe <chanuka.s.wijesinghe@gmail.com>
2026-01-20 17:30:31 +0530

    initial

---
+++ Predicting Medical Charges/medical_predict.py
@@ -0,0 +1,16 @@
+# Linear Regression
+from urllib.request import urlretrieve
+import pandas as pd
+
+
+# Step 1: Import the dataset and save as a CSV.
+medical_charges_url = "https://raw.githubusercontent.com/JovianML/opendatasets/master/data/medical-charges.csv"
+urlretrieve(medical_charges_url, 'medical.csv')
+
+# Step 2: Create a Pandas dataframe to view and analyse the data
+medical_df = pd.read_csv('medical.csv')
+# print(medical_df)
+
+# medical_df.info() # Check the data types
+stat_info = medical_df.describe() # Statistical summary
+print(stat_info)
