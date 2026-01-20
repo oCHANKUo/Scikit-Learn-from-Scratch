1c4d388f340773d996d33df56b307b1409eb9e61
Chanuka Wijesinghe <chanuka.s.wijesinghe@gmail.com>
2026-01-20 18:17:48 +0530

    Visualisation

--- Predicting Medical Charges/medical_predict.py
+++ Predicting Medical Charges/medical_predict.py
@@ -6,7 +6,6 @@ import matplotlib
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-
 # Step 1: Import the dataset and save as a CSV.
 medical_charges_url = "https://raw.githubusercontent.com/JovianML/opendatasets/master/data/medical-charges.csv"
 urlretrieve(medical_charges_url, 'medical.csv')
@@ -17,4 +16,26 @@ medical_df = pd.read_csv('medical.csv')
 
 # medical_df.info() # Check the data types
 stat_info = medical_df.describe() # Statistical summary
-print(stat_info) 
+# print(stat_info) 
+
+# Step 3: Visualisation
+# Adjusting default style settings
+sns.set_style('darkgrid')
+matplotlib.rcParams['font.size'] = 14
+matplotlib.rcParams['figure.figsize'] = (10, 6)
+matplotlib.rcParams['figure.facecolor'] = '#00000000'
+
+fig, (ax1, ax2) = plt.subplots(2, 1)
+# Visualise ages in a histogram since age is numeric. Use matplotlib
+ax1.hist(medical_df['age'], bins=47, rwidth=0.9) # bins to account for all the ages 18-64. relative width between each bar
+ax1.set_title('Distribution of Age')
+ax1.set_xlabel('Age')
+ax1.set_ylabel('Count')
+
+# Boxplot
+ax2.boxplot(medical_df['age'], vert=False)
+ax2.set_title('Age Box Plot')
+ax2.set_xlabel('Age')
+
+plt.tight_layout()
+plt.show()
