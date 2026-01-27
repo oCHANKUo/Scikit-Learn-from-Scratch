10e0bb4d8b7668f7295cc8b0133b0107c8f5a532
Chanuka Wijesinghe <chanuka.s.wijesinghe@gmail.com>
2026-01-27 13:08:49 +0530

    standardisation

--- Predicting Medical Charges/standardization_lr_sklearn.py
+++ Predicting Medical Charges/standardization_lr_sklearn.py
@@ -17,5 +17,63 @@ from linear_regression import try_parameter, rmse
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import SGDRegressor
 from sklearn import preprocessing
+from sklearn.preprocessing import StandardScaler
 
 medical_df = pd.read_csv("medical.csv")
+
+# normal categorisation as before
+smoker_codes = {'no' : 0, 'yes' : 1}
+medical_df['smoker_code'] = medical_df.smoker.map(smoker_codes)
+sex_codes = {'female' : 0, 'male' : 1}
+medical_df['sex_code'] = medical_df.sex.map(sex_codes)
+enc = preprocessing.OneHotEncoder()
+enc.fit(medical_df[['region']])
+one_hot = enc.transform(medical_df[['region']]).toarray()
+medical_df[['northeast', 'northwest', 'southeast', 'southwest']] = one_hot
+
+
+numeric_cols = ['age', 'bmi', 'children']
+scaler = StandardScaler()
+scaler.fit(medical_df[numeric_cols])
+
+# scale the data
+scaled_inputs = scaler.transform(medical_df[numeric_cols])
+
+cat_cols = ['smoker_code', 'sex_code', 'northeast', 'northwest', 'southeast', 'southwest']
+categorical_data = medical_df[cat_cols].values
+
+inputs = np.concatenate((scaled_inputs, categorical_data), axis=1)
+targets = medical_df.charges
+
+model = LinearRegression()
+model.fit(inputs, targets)
+
+predictions = model.predict(inputs)
+loss = rmse(targets, predictions)
+
+if __name__ == "__main__":
+    # print(scaler.mean_) # mean
+    # print(scaler.var_) # variance/standard deviation
+    # print(scaled_inputs)
+    print(predictions)
+    print(loss)
+
+
+    print('----------------')
+
+
+'''
+Scaling does not effect the Loss. Instead it changes the Weights.
+When testing for values, they need to be scaled as well
+'''
+
+
+'''
+Finally, How to approach a machine learning problem
+1. Explore the data and find correlations between input - target
+2. Pick the right model, loss functions and optimzer
+3. Scale numeric variables and one hot encode categorical data
+4. Set aside a test set
+5. Train model
+6. Make predictions on the test set
+'''
