452b0cca81b9338946b4895d61e653166564ebef
Chanuka Wijesinghe <chanuka.s.wijesinghe@gmail.com>
2026-01-27 11:18:33 +0530

    smoker predicitons

--- Predicting Medical Charges/single_feature_linear_regression_sklearn.py
+++ Predicting Medical Charges/single_feature_linear_regression_sklearn.py
@@ -12,23 +12,26 @@ from sklearn.linear_model import SGDRegressor
 
 medical_df = pd.read_csv("medical.csv")
 non_smoker_df = medical_df[medical_df.smoker == 'no']
+smoker_df = medical_df[medical_df.smoker == 'yes']
 
 # Initializes a Linear Regression model
 model = LinearRegression()
 model2 = SGDRegressor()
 
 inputs = non_smoker_df[['age']] # 2 [[]], because model.fit requires 2 dimensional arrays
+smoker_inputs = smoker_df[['age']]
 targets = non_smoker_df.charges # what i want to predict
+smoker_targets = smoker_df.charges
 
 # trains the model
 # model.fit(inputs, targets) 
-model2.fit(inputs, targets)
+model2.fit(smoker_inputs, smoker_targets)
 
 # predictions = model.predict(np.array([[23],
 #                                       [37],
 #                                       [61]]))
 
-predictions = model2.predict(inputs)
+predictions = model2.predict(smoker_inputs)
 
 # w
 w = model2.coef_
@@ -37,7 +40,7 @@ w = model2.coef_
 b = model2.intercept_
 
 if __name__ == "__main__":
-    print(predictions)
+    # print(predictions)
     print(w)
     print(b)
     try_parameter(w, b)
