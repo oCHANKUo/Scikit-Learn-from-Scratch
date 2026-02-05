91a260940cad1044bbce560aa18e7875e002a461
Chanuka Wijesinghe <chanuka.s.wijesinghe@gmail.com>
2026-02-05 15:06:35 +0530

    preprocessing done

--- Decision Trees/decision_trees_note.md
+++ Decision Trees/decision_trees_note.md
@@ -12,10 +12,30 @@ https://www.kaggle.com/jsphyg/weather-dataset-rattle-package
 ## Preprocessing Data
 1. Create a train/test/validation split
 > it's often a good idea to separate the training, validation and test sets with time, so that the model is trained on data from the past and evaluated on data from the "future".
+
 2. Identify input and target columns
 > Input: All columns except for first(date) and last(RainTomorrow) columns
 > Target: Last, RainTomorrow column
+
 3. Identify numeric and categorical columns
+> numeric_cols = train_inputs.select_dtypes(include=np.number).columns.tolist()
+> categorical_cols = train_inputs.select_dtypes('object').columns.tolist()
+
 4. Impute (fill) missing numeric values
+> from sklearn.impute import SimpleImputer
+
 5. Scale numeric values to the (0, 1) range
+> from sklearn.preprocessing import MinMaxScaler
+> This step is not necessary for Categorical Data
+
 6. Encode categorical columns to one-hot-vectors
+> from sklearn.preprocessing import OneHotEncoder
+> Concatanation (*pd.concat*) instead of normal assignment is better for avoiding memory fragmentation
+> ex: train_encoded = pd.DataFrame(
+            encoder.transform(train_inputs[categorical_cols]),
+            columns=encoded_cols,
+            index=train_inputs.index
+        )
+        train_inputs = pd.concat([train_inputs, train_encoded], axis=1)
+
+7. drop the textual categorical columns, so that we're left with just numeric data. Such as Location name
--- Decision Trees/preprocessing.py
+++ Decision Trees/preprocessing.py
@@ -5,6 +5,9 @@ import plotly.express as px
 import matplotlib
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
 
 data_dir = 'weather-dataset-rattle-package'
 train_csv = data_dir + '/weatherAUS.csv'
@@ -25,18 +28,71 @@ target_cols = 'RainTomorrow'
 
 train_inputs = train_df[input_cols].copy()
 train_targets = train_df[target_cols].copy()
-val_inputs = train_df[input_cols].copy()
-val_targets = train_df[target_cols].copy()
-train_inputs = train_df[input_cols].copy()
-train_targets = train_df[target_cols].copy()
+val_inputs = val_df[input_cols].copy()
+val_targets = val_df[target_cols].copy()
+test_inputs = test_df[input_cols].copy()
+test_targets = test_df[target_cols].copy()
+
+# Numeric and Categorical Columns
+numeric_cols = train_inputs.select_dtypes(include=np.number).columns.tolist()
+categorical_cols = train_inputs.select_dtypes(include='string').columns.tolist()
+
+## Imputing
+imputer = SimpleImputer(strategy='mean').fit(train_inputs[numeric_cols])
+train_inputs[numeric_cols] = imputer.transform(train_inputs[numeric_cols])
+val_inputs[numeric_cols] = imputer.transform(val_inputs[numeric_cols])
+test_inputs[numeric_cols] = imputer.transform(test_inputs[numeric_cols])
+
+# Scaling
+scaler = MinMaxScaler().fit(train_inputs[numeric_cols])
+train_inputs[numeric_cols] = scaler.transform(train_inputs[numeric_cols])
+val_inputs[numeric_cols] = scaler.transform(val_inputs[numeric_cols])
+test_inputs[numeric_cols] = scaler.transform(test_inputs[numeric_cols])
+
+# Encoding
+encoder = OneHotEncoder(sparse_output=False, handle_unknown='ignore').fit(train_inputs[categorical_cols])
+encoded_cols = list(encoder.get_feature_names_out(categorical_cols))
+train_encoded = pd.DataFrame(
+    encoder.transform(train_inputs[categorical_cols]),
+    columns=encoded_cols,
+    index=train_inputs.index
+)
+train_inputs = pd.concat([train_inputs, train_encoded], axis=1)
+
+val_encoded = pd.DataFrame(
+    encoder.transform(val_inputs[categorical_cols]),
+    columns=encoded_cols,
+    index=val_inputs.index
+)
+val_inputs = pd.concat([val_inputs, val_encoded], axis=1)
+
+test_encoded = pd.DataFrame(
+    encoder.transform(test_inputs[categorical_cols]),
+    columns=encoded_cols,
+    index=test_inputs.index
+)
+test_inputs = pd.concat([test_inputs, test_encoded], axis=1)
+
+# Drop Textual Categorical Data
+X_train = train_inputs[numeric_cols + encoded_cols]
+X_val = val_inputs[numeric_cols + encoded_cols]
+X_test = test_inputs[numeric_cols + encoded_cols]
 
 if __name__ == "__main__":
 
     # raw_df.info()
 
-    print('train_df.shape: ', train_df.shape)
-    print('val_df.shape: ', val_df.shape)
-    print('test_df.shape: ', test_df.shape)
+    # print('train_df.shape: ', train_df.shape)
+    # print('val_df.shape: ', val_df.shape)
+    # print('test_df.shape: ', test_df.shape)
 
+    # print(numeric_cols)
+    # print(categorical_cols)
+
+    # print(test_inputs[numeric_cols].isna().sum()) # Check missing values
+
+    # print(val_inputs.describe().loc[['min', 'max']]) # Check min mac values for each columns. Should be between 0 and 1
+    
+    print(X_test)
 
     print('-------------------------')
