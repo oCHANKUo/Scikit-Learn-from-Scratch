add4bd0bfbc8c8d87dcba25773562a002468c5bf
Chanuka Wijesinghe <chanuka.s.wijesinghe@gmail.com>
2026-02-24 11:41:23 +0530

    pre train

--- Rossman Store Sales Problem/training.py
+++ Rossman Store Sales Problem/training.py
@@ -2,9 +2,56 @@ import opendatasets as od
 import os
 import pandas as pd
 from data_prep import ross_df, merged_df, merged_test_df
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
 
+train_size = int(.75 * len(merged_df))
+
+sorted_df = merged_df.sort_values('Date')
+train_df, val_df = sorted_df[:train_size], sorted_df[train_size:]
+
+input_cols = ['Store', 'DayOfWeek', 'Promo', 'StateHoliday', 'StoreType', 'Assortment', 'Day', 'Month', 'Year']
+target_col = 'Sales'
+
+train_inputs = train_df[input_cols].copy()
+train_targets = train_df[target_col].copy()
+
+val_inputs = val_df[input_cols].copy()
+val_targets = val_df[target_col].copy()
+
+test_inputs = merged_test_df[input_cols].copy()
+
+numeric_cols = ['Store', 'Day', 'Month', 'Year']
+categorical_cols = ['DayOfWeek', 'Promo', 'StateHoliday', 'StoreType', 'Assortment']
+
+'''Imputation'''
+imputer = SimpleImputer(strategy='mean').fit(train_inputs[numeric_cols])
+train_inputs[numeric_cols] = imputer.transform(train_inputs[numeric_cols])
+val_inputs[numeric_cols] = imputer.transform(val_inputs[numeric_cols])
+test_inputs[numeric_cols] = imputer.transform(test_inputs[numeric_cols])
+
+'''Scaling'''
+scaler = MinMaxScaler().fit(train_inputs[numeric_cols])
+train_inputs[numeric_cols] = scaler.transform(train_inputs[numeric_cols])
+val_inputs[numeric_cols] = scaler.transform(val_inputs[numeric_cols])
+test_inputs[numeric_cols] = scaler.transform(test_inputs[numeric_cols])
+
+'''Encoding'''
+encoder = OneHotEncoder(sparse=False, handle_unknown='ignore').fit(train_inputs[categorical_cols])
+encoded_cols = list(encoder.get_feature_names_out(categorical_cols))
+
+train_inputs[encoded_cols] = encoder.transform(train_inputs[categorical_cols])
+val_inputs[encoded_cols] = encoder.transform(val_inputs[categorical_cols])
+test_inputs[encoded_cols] = encoder.transform(test_inputs[categorical_cols])
+
+'''Extract out numeric data'''
+X_train = train_inputs[numeric_cols + encoded_cols]
+X_val = val_inputs[numeric_cols + encoded_cols]
+X_test = test_inputs[numeric_cols + encoded_cols]
 
 if __name__ == "__main__":
 
-    print(len(merged_df))
+    # print(len(merged_df))
+    # print(len(train_df), len(val_df))
+    # print(train_df.columns)
     print('------------------')
